600f9cc841df67476ca3b528903e90124afade32
bartwr <mail@bartroorda.nl>
2022-09-06 22:26:52 +0200

    Update code syntax

--- index.js
+++ index.js
@@ -2,32 +2,39 @@ import http from 'http';
 import spauth from 'node-sp-auth';
 import requestprom from 'request-promise';
 import {username, password} from './config.js';
+
 // Site and User Creds  
 const url = 'https://ammegagroup.sharepoint.com';
 const server = http.createServer(function(request, response) {  
   // Authenticate with hardcoded credentials  
   spauth.getAuth(url, {          
-    username:username,  
-    password:password  
+    username: username,
+    password: password  
   })  
   .then(function(options){  
     // Headers  
     var headers = options.headers;
     headers['Accept'] = 'application/json;odata=verbose';
-    // Pull the SharePoint list items  
     requestprom.get({  
-    url: url+"/_api/web/lists/getByTitle('Documents')/items",  
-        headers: headers,  
-        json: true  
+      // This works:
+      url: url+"/_api/web/GetFolderByServerRelativeUrl('/Shared Documents')",
+      // This doesn't:
+      // url: url+"/_api/web/GetFolderByServerRelativeUrl('/DataSheet')",
+      headers: headers,  
+      json: true  
     }).then(function(listresponse){
+      console.log('listresponse', listresponse)
+      return;
+
+      // Later we will loop items and such ->
       var items = listresponse.d.results;
       var responseJSON = [];
       // process  
       items.forEach(function(item) {  
         console.log('item', item)
-          if(item.Title !=null){  
-              responseJSON.push(item.Title);
-          }                     
+        if(item.Title !=null){  1
+          responseJSON.push(item.Title);
+        }                     
       }, this);
       // Print / Send back the data  
       response.end(JSON.stringify(responseJSON));
